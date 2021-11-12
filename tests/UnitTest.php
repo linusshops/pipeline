39279161cd3baff554c722ba75ea0afa2cf6eeb8
waterloomatt <waterloomatt@gmail.com>
2021-11-11 19:16:50 -0500

    Add first test.

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
+require_once(__DIR__ . '/../src/Pipeline.php');
+
 class PipelineTest extends TestCase
 {
     public function testFirst()
     {
-        $this->assertEquals(1, 1);
+        $document = new StdClass();
+        $document->title = 'Test Title';
+        $document->author = 'Test Author';
+        $document->body = 'Test Body';
+
+        $output = (new Pipeline())
+            ->send($document)
+            ->through([])
+            ->via('transform')
+            ->thenReturn();
+
+        $this->assertEquals($document, $output);
     }
 }
